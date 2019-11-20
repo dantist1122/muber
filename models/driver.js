fac372b07d2f969e2d8a238bf92b9a2e9db8d1ea
Ivan Cerba <cerba.ivan@gmail.com>
2019-11-20 00:12:28 -0800

    erg

--- models/driver.js
+++ models/driver.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const PointSchema = new Schema({
+    type: {type: String, default: 'Point'},
+    coordinates: {type: [Number], index:'2dsphere'}
+});
+
 const DriverSchema = new Schema({
-    email:{
+    email: {
         type: String,
         required: true
     },
     driving: {
-        type:Boolean,
+        type: Boolean,
         default: false
-    }
+    },
+    geometry: PointSchema
 
 });
 
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,10 @@ const DriversController = require('../controllers/drivers_controllers');
 module.exports = (app) => {
 // watch for incoming request of method GET
 // to the route http://localhost:3050/api
+
     app.get('/api', DriversController.greeting);
+    app.post('/api/drivers', DriversController.create);
+    app.put('/api/drivers/:id', DriversController.edit);
+    app.delete('/api/drivers/:id', DriversController.delete);
+    app.get('/api/drivers', DriversController.index)
 };
